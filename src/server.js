de7bf79ac8bf64477f85fd671c9485c756dc74db
gabriel <llanesgabrielg@gmail.com>
2021-06-19 20:50:52 -0300

    consulta discos

--- src/server.js
+++ src/server.js
@@ -1,21 +1,61 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = 3000;
-const discos = require('./discos.json');
-// console.log(discos);
+//tengo un JSON
+let disks = require("./discos.json");
+//convierto a string y luego a objeto
+let strDisks = JSON.stringify(disks);
+let parseDisks = JSON.parse(strDisks);
+const vecDisks = parseDisks.discos;
 
 //middleware
-app.use(express.static(path.join(__dirname,'client')));
+app.use(express.static(path.join(__dirname, "client")));
 
 //endpoints - routes
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'client/klk.html'));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/klk.html"));
 });
-app.get('/disco', function (req, res) {
-  console.log(req.query);
-  res.send('hi my name is gabriel');
+
+app.get("/disco", function (req, res) {
+  //datos que llegan de un request
+  console.log(req.query); // tengo un objeto en el query
+  let resultFilter = vecDisks;
+  const reqTitle = req.query.title;
+  const reqArtist = req.query.artist;
+  const reqRelease = req.query.release;
+  //validacion y filtro de datos obtenidos para enviar una respuesta
+  if (reqTitle) {
+    resultFilter = resultFilter.filter(function (disk) {
+      if (disk.titulo.toLowerCase().includes(reqTitle.toLowerCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+  if (reqArtist) {
+    resultFilter = resultFilter.filter(function (disk) {
+      if (disk.artista.toLowerCase().includes(reqArtist.toLowerCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+  if (reqRelease) {
+    resultFilter = resultFilter.filter(function (disk) {
+      if (disk.lanzamiento === parseInt(reqRelease)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+  console.log(resultFilter);
+  res.json(resultFilter);
 });
 
 //server
